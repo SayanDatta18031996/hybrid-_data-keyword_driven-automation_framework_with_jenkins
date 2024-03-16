2be9ea5795dcafe1d7863659e5cdc02e1ad56b94
Sayan Datta <dattasayanbca@gmail.com>
2024-03-16 18:23:30 +0530

    base class started

---
+++ DataDrivenFramework/src/test/java/com/testbot/base/TestBase.java
@@ -0,0 +1,10 @@
+package com.testbot.base;
+
+public class TestBase {
+	/*
+	 * WebDriver - done Properties - done Logs - log4j jar, .log,
+	 * log4j.properties, Logger ExtentReports DB Excel Mail ReportNG,
+	 * ExtentReports Jenkins
+	 * 
+	 */
+}
